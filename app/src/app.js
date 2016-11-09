6f8336b3da552fb1942c35745218068a0f67ef25
Josh Kramer <jkjustjoshing@gmail.com>
2016-11-08 21:34:23 -0500

    Start playing around with Vue.js

--- app/src/app.js
+++ app/src/app.js
@@ -1,6 +1,37 @@
+Vue.component('vote-address-input', {
+  props: {
+    address: {
+      required: true,
+      type: Object
+    }
+  },
+  methods: {
+    update: function (event) {
+      this.$emit('change', this.address);
+    }
+  },
+
+  template: `
+    <div>
+      <input v-model="address.number" v-on:input="update">
+      <input v-model="address.street" v-on:input="update">
+    </div>
+  `
+});
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    address: {
+      number: 'foo',
+      street: 'bar'
+    }
+  },
+
+  methods: {
+    test: function (address) {
+      console.log('new address', address);
+    }
   }
 })
